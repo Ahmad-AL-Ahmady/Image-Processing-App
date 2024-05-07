e866aede77c9cf717dc1ba6f920481908c0a8ac1
Ahmad-AL-Ahmady <ahmad.hussain.alahmady@gmail.com>
2024-05-08 02:00:42 +0300

    Added mean filter

--- Python_Project_1.py
+++ Python_Project_1.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageTk
 from CTkMessagebox import CTkMessagebox
 
 set_appearance_mode("dark")
-set_default_color_theme("darkblue")
+set_default_color_theme("dark-blue")
 
 
 class ImageProcessing:
@@ -24,7 +24,10 @@ class ImageProcessing:
         self.load_image_button.pack()
 
         # Create IntVar for slider value
-        self.kernel_size_var = IntVar(value=5)
+        self.hpf_kernel_size_var = IntVar(value=5)
+        self.mean_kernel_size_var = IntVar(value=5)
+        self.median_kernel_size_var = IntVar(value=5)
+        self.roberts_kernel_size_var = IntVar(value=5)
         self.add_buttons_and_sliders()
 
     def load_default_image(self):
@@ -40,36 +43,58 @@ class ImageProcessing:
         self.image_label.pack()
 
     def add_buttons_and_sliders(self):
-        self.slider_label = CTkLabel(self.master,
-                                     text=f"HPF Kernel Size: {self.kernel_size_var.get()}")
-        self.slider_label.pack()
+        # HPF:
+        self.hpf_slider_label = CTkLabel(self.master,
+                                         text=f"HPF Kernel Size: {self.hpf_kernel_size_var.get()}")
+        self.hpf_slider_label.pack()
 
-        self.slider = CTkSlider(
+        self.hpf_slider = CTkSlider(
             self.master,
             from_=1,
             to=20,
-            command=self.update_slider_label,  # Update label on slider change
-            variable=self.kernel_size_var,
+            command=self.update_hpf_slider_label,  # Update label on slider change
+            variable=self.hpf_kernel_size_var,
             orientation=HORIZONTAL,
         )
-        self.slider.set(5)
-        self.slider.pack()
-
+        self.hpf_slider.set(5)
+        self.hpf_slider.pack()
         hpf_button = CTkButton(
             master=self.master, text="Apply HPF", command=self.apply_hpf)
         hpf_button.pack(pady=6)
 
-    def update_slider_label(self, event):
+        # MEAN:
+        self.mean_slider_label = CTkLabel(self.master,
+                                          text=f"Mean Kernel Size: {self.hpf_kernel_size_var.get()}")
+        self.mean_slider_label.pack()
+
+        self.mean_slider = CTkSlider(
+            self.master,
+            from_=1,
+            to=20,
+            command=self.update_mean_slider_label,  # Update label on slider change
+            variable=self.mean_kernel_size_var,
+            orientation=HORIZONTAL,
+        )
+        self.mean_slider.set(5)
+        self.mean_slider.pack()
+        mean_button = CTkButton(
+            master=self.master, text="Apply Mean", command=self.apply_mean)
+        mean_button.pack(pady=6)
+
+    def update_hpf_slider_label(self, event):
         # Update slider label to show current value
-        self.slider_label.configure(
-            text=f"Kernel Size: {self.kernel_size_var.get()}")
+        self.hpf_slider_label.configure(
+            text=f"HPF Kernel Size: {self.hpf_kernel_size_var.get()}")
+
+    def update_mean_slider_label(self, event):
+        self.mean_slider_label.configure(
+            text=f"Mean Kernel Size: {self.mean_kernel_size_var.get()}")
 
     def apply_hpf(self):
-        kernel_size = int(self.kernel_size_var.get())  # Get value from IntVar
+        kernel_size = int(self.hpf_kernel_size_var.get())
         if (kernel_size % 2 == 0):
             CTkMessagebox(title="Warning Message!", message="Kernel size must be odd in HPF.",
                           icon="warning", option_1="Ok")
-
         else:
             # Apply HPF using the kernel size
             gray_image = cv2.cvtColor(self.original_image, cv2.COLOR_BGR2GRAY)
@@ -77,6 +102,11 @@ class ImageProcessing:
                 gray_image, (kernel_size, kernel_size), 0)
             hpf_image = cv2.subtract(gray_image, blurred_image)
             self.update_image(cv2.cvtColor(hpf_image, cv2.COLOR_GRAY2BGR))
+
+    def apply_mean(self):
+        kernel_size = int(self.mean_kernel_size_var.get())
+        mean_image = cv2.blur(self.original_image, (kernel_size, kernel_size))
+        self.update_image(mean_image)
 
 
 root = CTk()
